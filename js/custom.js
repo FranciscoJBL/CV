10417afd72f9834efb7d8d83c8cc1d3dcf5fc73e
Francisco Briones <francisco.briones.l@gmail.com>
2018-02-06 17:20:16 -0300

    Auto stash before revert of "Set theme jekyll-theme-minimal"

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var change= false;
+$(window).scroll(function(){
+    window_y = $(window).scrollTop(); 
+    scroll_critical = parseInt($("#PRIMER_DIV").height()); 
+    if (window_y > scroll_critical) { 
+      
+    } else {
+
+    }
+});
